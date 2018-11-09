57a8ed6e4fd14d1188aeea0b76fa6b5ddf3a939e
LillaJuhasz <LillaJuhasz@users.noreply.github.com>
2018-11-09 08:54:37 +0100

    using MLPRegressor within a dataset of quadratic and sin functions

--- main.py
+++ main.py
@@ -1,23 +1,74 @@
-import csv
-import sklearn
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-
-from sklearn.neural_network import MLPClassifier, MLPRegressor
+from sklearn.neural_network import MLPRegressor
 
 df = pd.read_csv('dataset.csv')
-y = list((df['output']).values)
+df = df.reset_index()
+X = np.asarray(list((df['input']).values))
+X_f = pd.np.reshape([float(i) for i in X], (-1, 1))
 
-X = list((df['input']).values)
-X_f= pd.np.reshape([float(i) for i in X], (-1, 1))
+y_quadratic = np.asarray(list((df['output1']).values))
+y_sin = np.asarray(list((df['output2']).values))
 
-clf = MLPRegressor(solver='lbfgs', alpha=1e-5, max_iter=1000, hidden_layer_sizes=(100), random_state=1)
+activations = ['identity', 'logistic', 'relu']
+solvers = ['sgd', 'adam', 'lbfgs']
+hidden_layers = [(100, 1), (20, 5, 100, 2)]
 
-clf.fit(X_f, y)
-pred=clf.predict(X_f)
-print(pred)
-plt.plot(X_f, y, 'o')
-plt.plot(X_f, pred, 'o')
-plt.show()
+clf = list()
+scores = []
+int_scores = []
 
-print(clf.score(X_f, y))
+a = 0
+for i in activations:
+    for j in solvers:
+        for k in hidden_layers:
+            clf.append(MLPRegressor(activation=i, solver=j, hidden_layer_sizes=k, random_state=1, max_iter=1000))
+
+            clf[a].fit(X_f, y_quadratic)
+            try:
+                score = clf[a].score(X_f, y_quadratic)
+            except ValueError:
+                score = 'Unsuccessful'
+
+            print('quadratic', score, i, j, k, sep='; ', file=open('scores.txt', 'a'))
+            if score != "Unsuccessful":
+                int_scores.append(score)
+
+            pred = clf[a].predict(X_f)
+            plt.plot(X_f, y_quadratic, 'o')
+            plt.plot(X_f, pred, 'o')
+            plt.savefig("plots/"+j + '' + i + 'quad.pdf')
+            plt.show()
+
+            a += 1
+
+a = 0
+for i in activations:
+    for j in solvers:
+        for k in hidden_layers:
+            clf.append(MLPRegressor(activation=i, solver=j, hidden_layer_sizes=k, random_state=1, max_iter=1000))
+
+            clf[a].fit(X_f, y_sin)
+            try:
+                score = clf[a].score(X_f, y_sin)
+            except ValueError:
+                score = 'Unsuccessful'
+
+            print('sin', score, i, j, k, sep='; ', file=open('scores.txt', 'a'))
+            if score != "Unsuccessful":
+                int_scores.append(score)
+
+            pred = clf[a].predict(X_f)
+            plt.plot(X_f, y_sin, 'o')
+            plt.plot(X_f, pred, 'o')
+            plt.savefig("plots/"+j + '' + i + 'sin.pdf')
+            plt.show()
+
+            a += 1
+
+
+print('average: ', np.average(int_scores))
+print('median: ', np.median(int_scores))
+print('minimum: ', np.min(int_scores))
+print('maximum: ', np.max(int_scores))
