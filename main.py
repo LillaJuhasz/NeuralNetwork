d446e08bd5cf8558ec09aa8d141a863cd8f100fc
LillaJuhasz <LillaJuhasz@users.noreply.github.com>
2018-11-11 21:46:35 +0100

    data frame added

--- main.py
+++ main.py
@@ -3,73 +3,79 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPRegressor
 from scipy import stats
+from pandas import DataFrame as df
 
-df = pd.read_csv('dataset.csv')
-df = df.reset_index()
-X = np.asarray(list((df['input']).values))
-X_f = pd.np.reshape([float(i) for i in X], (-1, 1))
+ds = pd.read_csv('dataset.csv')
+ds = ds.reset_index()
 
-y_quadratic = np.asarray(list((df['output1']).values))
-y_sin = np.asarray(list((df['output2']).values))
+input_data = np.asarray(list((ds['input']).values))
+input_float = pd.np.reshape([float(i) for i in input_data], (-1, 1))
+
+output_quadratic = np.asarray(list((ds['output1']).values))
+output_sin = np.asarray(list((ds['output2']).values))
 
 activations = ['identity', 'logistic', 'relu']
 solvers = ['sgd', 'adam', 'lbfgs']
 hidden_layers = [(100, 1), (1000, 10, 1000, 10)]
 
-clf = list()
+regressor = list()
 quadratic_scores = []
 sin_scores = []
 int_scores = []
 
-a = 0
+counter = 0
 for i in activations:
     for j in solvers:
         for k in hidden_layers:
-            clf.append(MLPRegressor(activation=i, solver=j, hidden_layer_sizes=k, random_state=1, max_iter=1000))
+            regressor.append(MLPRegressor(activation=i, solver=j, hidden_layer_sizes=k, random_state=1, max_iter=1000))
 
-            clf[a].fit(X_f, y_quadratic)
+            regressor[counter].fit(input_float, output_quadratic)
             try:
-                score = clf[a].score(X_f, y_quadratic)
+                score = regressor[counter].score(input_float, output_quadratic)
             except ValueError:
                 score = 'Unsuccessful'
+            pred = regressor[counter].predict(input_float)
 
             print('quadratic', score, i, j, k, sep='; ', file=open('scores.txt', 'a'))
             if score != "Unsuccessful":
                 quadratic_scores.append(score)
                 int_scores.append(score)
 
-            pred = clf[a].predict(X_f)
-            plt.plot(X_f, y_quadratic, 'o')
-            plt.plot(X_f, pred, 'o')
+            plt.plot(input_float, output_quadratic, 'o')
+            plt.plot(input_float, pred, 'o')
             plt.savefig("plots/"+j + '' + i + '' + str(hidden_layers.index(k)) + 'quad.pdf')
             plt.show()
 
-            a += 1
+            counter += 1
 
-a = 0
+counter = 0
 for i in activations:
     for j in solvers:
         for k in hidden_layers:
-            clf.append(MLPRegressor(activation=i, solver=j, hidden_layer_sizes=k, random_state=1, max_iter=1000))
+            regressor.append(MLPRegressor(activation=i, solver=j, hidden_layer_sizes=k, random_state=1, max_iter=1000))
 
-            clf[a].fit(X_f, y_sin)
+            regressor[counter].fit(input_float, output_sin)
             try:
-                score = clf[a].score(X_f, y_sin)
+                score = regressor[counter].score(input_float, output_sin)
             except ValueError:
                 score = 'Unsuccessful'
+            pred = regressor[counter].predict(input_float)
 
             print('sin', score, i, j, k, sep='; ', file=open('scores.txt', 'a'))
             if score != "Unsuccessful":
                 sin_scores.append(score)
                 int_scores.append(score)
 
-            pred = clf[a].predict(X_f)
-            plt.plot(X_f, y_sin, 'o')
-            plt.plot(X_f, pred, 'o')
+            plt.plot(input_float, output_sin, 'o')
+            plt.plot(input_float, pred, 'o')
             plt.savefig("plots/"+j + '' + i + '' + str(hidden_layers.index(k)) + 'sin.pdf')
             plt.show()
 
-            a += 1
+            counter += 1
 
 print('quadratic', stats.describe(quadratic_scores), sep='; ', file=open('stats.txt', 'a'))
 print('sin', stats.describe(sin_scores), sep='; ', file=open('stats.txt', 'a'))
+
+quadratic_scores.append(0.0)
+data = df(data={'quadratic': quadratic_scores, 'sin': sin_scores})
+print(data.describe(), sep='; ', file=open('stats.txt', 'a'))
